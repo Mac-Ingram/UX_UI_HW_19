99f86de07258473509685bfcb25cf422f599fd59
Mac-Ingram <70731094+Mac-Ingram@users.noreply.github.com>
2021-02-06 20:15:37 -0500

    week 19

---
+++ js/index.js
@@ -0,0 +1,25 @@
+console.log("This isn't working");
+
+$("li").hover(
+    function() {
+      $( this ).addClass("hover");
+    }, function() {
+      $( this ).removeClass( "hover");
+    }
+  );
+
+  $("button").hover(
+    function() {
+      $( this ).addClass("btnhover");
+    }, function() {
+      $( this ).removeClass( "btnhover");
+    }
+  );
+
+  $("connectbtn").hover(
+    function() {
+      $( this ).addClass("btnhover");
+    }, function() {
+      $( this ).removeClass( "btnhover");
+    }
+  );
